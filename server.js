68e526e8745a7fec0401ecddf4e5654f645340d0
Abhi Muktheeswarar <abhimuktheeswarar@Abhis-MacBook-Pro.local>
2018-10-02 12:01:58 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require(`express`);
+const hbs = require(`hbs`);
+const fs = require(`fs`);
+
+let app = express();
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.set(`view engine`, `hbs`);
+
+app.use((request, response, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${request.method} = ${request.url} \n`;
+  console.log(log);
+  fs.appendFile(`server.log`, log, (error) => {
+    console.log(error);
+  });
+  next();
+
+});
+
+app.use((request, response, next) => {
+
+  response.render(`maintenance.hbs`)
+
+});
+
+app.use(express.static(`${__dirname}/public`));
+
+hbs.registerHelper(`getCurrentYear`, () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper(`screamIt`, (text) => {
+  return text.toUpperCase();
+})
+
+app.get(`/`, (request, response) => {
+
+  //response.send(`<h1>Hello World!</h1>`);
+
+  // response.send({
+  //   title: `Hello World!`,
+  //   message: `Finally got it`
+  // })
+
+  response.render(`home.hbs`, {
+    pageTitle: `Welcome`,
+    description: `Hello World!`
+  });
+
+});
+
+app.get(`/about`, (request, response) => {
+
+  //response.send(`<h1>Hello World!</h1>`);
+  response.render(`about.hbs`, {
+    pageTitle: `About page`
+  });
+
+});
+
+app.get(`/error`, (request, response) => {
+
+  response.send({
+    errorMessage: `Error fetching nothing`
+  })
+})
+
+app.listen(3000, () => {
+  console.log(`Server is up on port 3000`);
+});
